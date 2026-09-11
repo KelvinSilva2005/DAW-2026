b68121505db925ea5d8b3d7e28f8c34aba456952
KelvinSilva2005 <kelvinlaranja22@gmail.com>
2026-09-11 20:12:06 -0300

    Aula 1109

---
+++ cardapio-digital/assets/js/script.js
@@ -0,0 +1,12 @@
+const categoryLinks = document.querySelectorAll('nav a');
+
+categoryLinks.forEach(function(link) {
+    
+});
+
+function defineComportamento( Link ) {
+    // Bloco de código
+    link.text = 'Olá Mundo';
+    link.cor = 'red';
+    link.barulho = 'click';
+}
