5a7e073c9dfc98318f0629bbdec3ac6307ab7372
KelvinSilva2005 <kelvinlaranja22@gmail.com>
2026-09-11 21:41:49 -0300

    js criado

--- cardapio-digital/assets/js/script.js
+++ cardapio-digital/assets/js/script.js
@@ -1,12 +1,37 @@
 const categoryLinks = document.querySelectorAll('nav a');
 
-categoryLinks.forEach(function(link) {
-    
-});
+console.log('Category Links:');
+console.log(categoryLinks);
 
-function defineComportamento( Link ) {
+/*function defineComportamento( Link ) {
     // Bloco de código
     link.text = 'Olá Mundo';
     link.cor = 'red';
     link.barulho = 'click';
-}
+}*/
+
+categoryLinks.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        // Desativa a tentativa de navegação do link
+        event.preventDefault();
+
+        // Pega o nome da categoria clicada
+        const categoryName = link.textContent;
+
+        // Atualiza o texto da categoria selecionada
+        selectedCategory.textContent = categoryName;
+
+        alert('Categoria clicada:', categoryName);
+
+        // Remover a claas "active"
+        categoryLinks.forEach(function(CategoryLink) {
+            CategoryLink.classList.remove('active');
+        });
+
+        // Adicionar a classe "active" ao link clicado
+        link.classList.add('active');
+        
+    });
+});
